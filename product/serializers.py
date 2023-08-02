4199132c8daf2a338a986048e04f8290570930d7
Jessica Unicorn <jessicawangcloud@gmail.com>
2023-08-01 23:04:13 -0700

    serializers: make image_name optional(not required)

--- product/serializers.py
+++ product/serializers.py
@@ -5,7 +5,7 @@ class ProductDetailSerializer(serializers.Serializer):
 	id = serializers.IntegerField(read_only=True)
 	name = serializers.CharField(max_length=255)
 	description = serializers.CharField()
-	image_name = serializers.CharField(max_length=255)
+	image_name = serializers.CharField(max_length=255, required=False)
 
 	class Meta:
 		model = ProductDetail
