026d028ae43b6bb472d909acd4787c625237708a
Jessica Unicorn <jessicawangcloud@gmail.com>
2023-08-01 23:08:20 -0700

    support POST {{base_url}}/api/products/create/

--- product/urls.py
+++ product/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/products/', views.product_list, name="product_list"),
-    path('api/products/<int:product_id>/', views.product_detail, name='product_detail'),
+    path('api/products/', views.get_product_list, name="get_product_list_url"),
+    path('api/products/<int:product_id>/', views.get_product_detail, name='get_product_detail_url'),
+    path('api/products/create/', views.create_product, name="create_product_url"),
 ]
--- product/views.py
+++ product/views.py
@@ -4,13 +4,22 @@ from .models import ProductDetail
 from .serializers import ProductDetailSerializer
 
 @api_view(['GET'])
-def product_list(request):
+def get_product_list(request):
     query_set = ProductDetail.objects.all()
     serializer = ProductDetailSerializer(query_set, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
-def product_detail(request, product_id):
+def get_product_detail(request, product_id):
     single_product = ProductDetail.objects.get(id=product_id)
     serializer = ProductDetailSerializer(single_product)
     return Response(serializer.data)
+
+@api_view(['POST'])
+def create_product(request):
+    newItem = ProductDetail.objects.create(
+        name=request.data["name"],
+        description=request.data["description"]
+    )
+    serializer = ProductDetailSerializer(instance=newItem)
+    return Response(serializer.data, status=201) 
